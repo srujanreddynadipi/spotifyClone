2eb23f4c5c9a941b0c19bded855c1534c0ad0bfc
classroom <srujanreddynsreddy@gmail.com>
2025-04-16 11:40:30 +0530

    `Refactored Sidebar component with new CSS styles and JSX implementation`

---
+++ src/components/sidebar/Cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Cards = (
+  {
+   heading,
+   subheading,
+   buttontext, 
+  }
+) => {
+  return <div className="sideBarMiddleCards">
+    <h1>{heading}</h1>
+    <p>{subheading}</p>
+    <button>{buttontext}</button>
+  </div>;
+};
+
+export default Cards;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import "./Sidebar.css";
+import Cards from "./Cards";
 
 const Sidebar = () => {
+  const texts = [
+    "Legal ",
+    "Security",
+    "Privacy",
+    "Cookies",
+    "Advertising",
+    "About",
+    "Help",
+  ];
   return (
     <div className="sidebarMainContainer">
       <div className="sideBarTopSection">
-        
+        <h1>Your Library</h1>
+        <p>+</p>
       </div>
-      <div className="sideBarMiddleSection"></div>
-      <div className="sideBarBottomSection"></div>
+      <div className="sideBarMiddleSection">
+        <Cards
+          heading="Create your first playlist"
+          subheading="it's easy we  will help you "
+          buttontext="Create Playlist"
+        />
+        <Cards
+          heading="Lets find some podcast to follow"
+          subheading="will keep you updated on new episodes"
+          buttontext="Browse Podcast"
+        />
+      </div>
+      <div className="sideBarBottomSection">
+        <div className="sideBarBottomSectionTopContainer">
+          {texts.map((ele) => {
+            return <p>{ele}</p>;
+          })}
+          <p>Legal</p>
+        </div>
+        <div className="sideBarBottomSectionBottomContainer"> </div>
+      </div>
     </div>
   );
 };
